91ea612f7c4add961d3fef7e3a2f0814340495b3
Tristan Gates <56215102+tristan-gates@users.noreply.github.com>
2022-09-16 11:47:15 -0500

    first commit

---
+++ Lab1.py
@@ -0,0 +1,13 @@
+# Lab 1
+# Tristan Gates
+# Fall 2018
+# Create a code that details how to get to room 257 from outside of Ayers Hall.
+
+print ("Open the front door of Ayers Hall.")
+print ("Close door. Walk up the flight of stairs to the second floor.")
+print ("Once on the second floor, take a right down the hallway.")
+print ("After reaching the end of the hallway, take a left.")
+print ("Walk until you see room 257.")
+print ("Once you see the door to 257, enter the code to unlock the door.")
+print ("If successfully unlocked, open the door.")
+print ("Congrats! You are in room 257 now.")
